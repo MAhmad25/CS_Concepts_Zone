d0a37798b493f51086d94e652fe70b1f709ac196
MAhmad25 <userarenotproduct@gmail.com>
2026-02-25 16:02:37 +0500

    remove some loading state

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./reducers/authSlice";
-import loadingSlice from "./reducers/loadingSlice";
 import postsSlice from "./reducers/postsSlice";
 const store = configureStore({
       reducer: {
             auth: authSlice,
-            loader: loadingSlice,
             posts: postsSlice,
       },
 });
