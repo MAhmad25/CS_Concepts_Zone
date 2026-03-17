cccb632f584a2e59c8875f6fec885d47de06dce4
MAhmad25 <userarenotproduct@gmail.com>
2026-03-17 17:59:26 +0500

    Minor changed

--- src/config/secret.js
+++ src/config/secret.js
@@ -1,6 +1,6 @@
 const secret = {
       supabase_url: import.meta.env.VITE_SUPABASE_URL,
-      supabase_anon_key: import.meta.env.VITE_SUPABASE_ANON_KEY,
+      supabase_anon_key: import.meta.env.VITE_SUPABASE_ANON,
       article_table: "Posts",
       image_bucket: "PostsImages",
 };
